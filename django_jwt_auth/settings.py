a7b0ca648c5cdfefcca2aff16a341b99c50c4a76
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-02-21 15:33:02 +0545

    Initiated virtual environment

--- django_jwt_auth/settings.py
+++ django_jwt_auth/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'auth_app.User'
+
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
