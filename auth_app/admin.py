2fea040d812c118fb343450928ea3f3d07625280
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-02-21 19:34:29 +0545

    Customized admin model

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'is_staff', 'is_active', 'is_superuser']
+    list_filter = ['id' ,'username', 'is_staff', 'is_active']
+    fieldsets = (
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'date_joined', 'last_login')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')
+        }),
+        ('User Credential', {
+            'fields': ('username', 'password')
+        })
+    )
+
+    search_fields = ['username']
